60f3a125626b605d6b144fb39132a57022602d84
Katy Atchison <katy.atchison20@gmail.com>
2025-03-26 21:49:31 -0500

    navbar styling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,26 +1,34 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 export const Navigation = () => (
-  <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand as={Link} to="/">
-        Zitec USA
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/about">
-            About
-          </Nav.Link>
-          <Nav.Link as={Link} to="/contact">
-            Contact
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
+  <Navbar expand="sm" className="bg-body-tertiary px-3">
+    <Navbar.Brand as={Link} to="/" className="mx-3">
+      <img
+        src="/src/assets/zitec-logo-only.png"
+        width="75"
+        height="75"
+        alt="Zitec USA Logo"
+        className="mx-2"
+      />
+      ZITEC USA
+    </Navbar.Brand>
+
+    <div className="flex-grow-1 flex-fill"></div>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse
+      id="basic-navbar-nav"
+      className="me-auto justify-content-evenly"
+    >
+      <Nav.Link as={Link} to="/about">
+        About Us
+      </Nav.Link>
+      <Nav.Link as={Link} to="/contact">
+        Our Products
+      </Nav.Link>
+      <Nav.Link as={Link} to="/contact">
+        Contact Us
+      </Nav.Link>
+    </Navbar.Collapse>
   </Navbar>
 );
